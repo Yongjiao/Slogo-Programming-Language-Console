2b044866cee4dd6c8618c7df0c68c88851fe476c
ar293in2015 <ar293@duke.edu>
2015-02-22 18:31:09 -0500

    RICHARD please complete these commands

--- src/commands/IfConditions.java
+++ src/commands/IfConditions.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import application.CommandFactory;
 
 public class IfConditions extends CommandFactory{
-	//TODO
+	//TODO: RICHARD PLEASE FIX
 	public void IFELSE(int expr, ArrayList<String> commIf, ArrayList<String> commElse){
 		if(expr != 0){
 			//exe commIf
