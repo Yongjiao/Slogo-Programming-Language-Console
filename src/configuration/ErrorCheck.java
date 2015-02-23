a3f0d0b1a51285bc368bf16b68e69898aeac8719
Yongjiao <yongjiao.yu@duke.edu>
2015-02-23 14:47:46 -0500

    loop command validation

--- src/configuration/ErrorCheck.java
+++ src/configuration/ErrorCheck.java
@@ -5,48 +5,80 @@ import java.util.HashMap;
 import java.util.regex.*;
 
 public class ErrorCheck {
-	/*  Other approach
-	private String regix_basic = "^(FD|FORWARD|BACK|BK|)\\s+\\d+";
-	private String regix_arithematic = "^(TOWARDS|SETXY|SUM|+|DIFFERENCE|-|PRODUCT|*|QUOTIENT|/|REMAINDER|%)\\s+\\d+\\s+\\d+"; //+ match one or more
-	private String regix_boolean = "^(LESS?|LESSP|GREATER?|GREATERP|EQUAL? |EQUALP|NOTEQUAL?|NOTEQUALP)";
-	private String regix_comment = "^#.*";
-	private String regix_degree = "^(LEFT|LT|RIGHT|RT|SETHEADING|SETH|SIN|COS|TAN|ATAN)\\s+\\d+"; // degrees>360			
-	private ArrayList <String> regixList = new ArrayList<String>();
-	*/
-	private HashMap<String, String> commandMap; //what if new commands added
+	private HashMap<String, String> commandMap;
+	private final String onenum = "\\s\\d+"; //one parameter only exactly one space between parameters
+	private final String twonum = "\\s\\d+\\s\\d+";	//two parameter
+	private final String com_regix = "\\s\\[.*\\]"; //[command]
+	private final String variable = ":\\w+";
+	private final String constant = "-?\\d+.?\\d*";
+	private final String commandname = "\\w+[?]?";
+	
+	private final String[] command = new String[]{"fd", "forward", "back", "bk", "towards", "tw", "setxy", "sum", "+", "difference","-", "product","*",
+			"quotient","remainder", "%", "/","#","left", "lf", "right", "rt", "setheading", "seth", "sin", "cos", "tan", "atan", "repeat", "dotimes"};
+	
+	String[] regix = new String[]{ "^fd"+ onenum,"^foward" + onenum, "^back" +onenum, "^bk"+ onenum, "^towards"+twonum, "^tw" + twonum, "^setxy" + twonum,
+			"^sum" + twonum, "^+" + twonum, "^difference"+ twonum, "^-" + twonum, "^product" + twonum, "^*" + twonum, "^quotient" + twonum,
+			"^remainder" + twonum, "^%" + twonum, "^/" + twonum, "^#.*", "^left" +onenum,"^lt" +onenum, "^right" +onenum,"^rt" +onenum, "setheading" +onenum,
+			"^seth" +onenum,"^sin" +onenum,"^cos" +onenum, "^tan" +onenum, "^atan" +onenum, "repeat"+onenum+"\\s\\[.*\\]", 
+			"dotimes"+ "\\s\\[\\s"+variable+"\\s\\d+\\s\\]"+com_regix};
+	
+/*
+	public boolean validateInput(String in){
+		String s = in.trim().toLowerCase();
+		String command = s.split(" ")[0];	
+		System.out.println(in);		
+	}
+*/
 
 	//non-nested command validation	
-	public boolean validateInput(String in){
+	public boolean validateBasicCommands(String in){
 		String s = in.trim().toLowerCase();
 		String command = s.split(" ")[0];
 		System.out.println(in);
 		String commandRegix = commandMap.get(command);
 		if(commandRegix != null){
-			//System.out.println(commandRegix);
 			return s.matches(commandRegix);
 			//call parser here.
 		}
 		return false;
 	}
+	
+	public boolean validateLoop(String in){
+		String s = in.trim().toLowerCase();
+		String command = s.split(" ")[0];
+		System.out.println(in);
+		String commandRegix = commandMap.get(command);//switch cases
+		String com = "\\[(.*?)\\]";
+		if(commandRegix != null && in.matches(commandRegix)){ //pass general syntax check
+			switch(command){
+			case "repeat":{
+				Pattern p = Pattern.compile(com);
+				Matcher m = p.matcher(in);
+				while(m.find())
+					return validateBasicCommands(m.group(1));
 
-	
-	
-	//parse after error check
-	private String getStringNoSpaces(String s) {
-		return s.replaceAll("\\s+", "");
+			}
+			//m.group(m.groupCount());
+			case "dotimes":{
+				Pattern p = Pattern.compile(com);
+				Matcher m = p.matcher(in);
+				while(m.find()){	
+					String bc = m.group();
+					System.out.println(m.group());
+					//return validateBasicCommands(bc);
+				}
+				}
+			case "for":{}
+			case "if":{}
+			case "ifelse":{}
+			}
+		}
+		return false;
 	}
 
+
 	public ErrorCheck(){
-		commandMap = new HashMap<String,String>();
-		String regix1 = "\\s\\d+"; //one parameter only exactly one space between parameters
-		String regix2 = "\\s\\d+\\s\\d+";	//two parameter
-		String[] command = new String[]{"fd", "forward", "back", "bk", "towards", "tw", "setxy", "sum", "+", "difference","-", "product","*",
-				"quotient","remainder", "%", "/","#.*","left", "lf", "right", "rt", "setheading", "seth", "sin", "cos", "tan", "atan"};
-		String[] regix = new String[]{ "^fd"+ regix1,"^foward" + regix1, "^back" +regix1, "^bk"+ regix1, "^towards"+regix2, "^tw" + regix2, "^setxy" + regix2,
-				"^sum" + regix2, "^+" + regix2, "^difference"+ regix2, "^-" + regix2, "^product" + regix2, "^*" + regix2, "^quotient" + regix2,
-				"^remainder" + regix2, "^%" + regix2, "^/" + regix2, "#//.*"+".*", "^left" +regix1,"^lt" +regix1, "^right" +regix1,"^rt" +regix1, "setheading" +regix1,
-				"^seth" +regix1,"^sin" +regix1,"^cos" +regix1, "^tan" +regix1, "^atan" +regix1};
-		System.out.println("what is the size difference"+ command.length + regix.length);
+		commandMap = new HashMap(); //what if new commands added
 		for(int i=0; i < command.length; i++){
 			commandMap.put(command[i],regix[i]);			
 		}
@@ -56,17 +88,21 @@ public class ErrorCheck {
 		ErrorCheck example = new ErrorCheck();
 		String s1 = "fd a";
 		String s2= "sum 50 50";
-		String s4 = "#.*1"; //comment not working: to be fixed
+		String s4 = "# ignore this is just comment!"; //broken when no space after #
 		String s3 = "sum a b";
 		String s5 = "REmainder 50 50";
 		String s6 = " rt 50   ";
 		String s7= "setheading 30";
-		System.out.println(example.validateInput(s1));
-		System.out.println(example.validateInput(s2));
-		System.out.println(example.validateInput(s3));
-		System.out.println(example.validateInput(s4));
-		System.out.println(example.validateInput(s5));
-		System.out.println(example.validateInput(s6));
-		System.out.println(example.validateInput(s7));
+		String repeat = "repeat 10 [ fd 50 ]";
+		String dotimes = "dotimes [ :name 200 ] [ rt 50 ]";
+		//System.out.println(example.validateBasicCommands(s1));
+		//System.out.println(example.validateBasicCommands(s2));
+		//System.out.println(example.validateBasicCommands(s3));
+		//System.out.println(example.validateBasicCommands(s4));
+		//System.out.println(example.validateBasicCommands(s5));
+		//System.out.println(example.validateBasicCommands(s6));
+		//System.out.println(example.validateBasicCommands(s7));
+		//System.out.println(example.validateLoop(repeat));		
+		System.out.println(example.validateLoop(dotimes));
 	}	
 }
