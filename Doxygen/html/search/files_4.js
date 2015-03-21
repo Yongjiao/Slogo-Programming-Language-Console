d9fa58933cb6e2077fd1ff6f989b0188f6db9565
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-21 22:42:18 +0000

    Added Readme

--- Doxygen/html/search/files_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['equal_2ejava',['Equal.java',['../d0/d89/_equal_8java.html',1,'']]],
-  ['executable_2ejava',['Executable.java',['../d7/ddf/_executable_8java.html',1,'']]]
-];
