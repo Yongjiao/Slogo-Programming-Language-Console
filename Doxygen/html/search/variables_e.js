86674aa03c3a2cb244d16cdff413849f224aa943
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-22 14:58:39 +0000

    Added Readme

--- Doxygen/html/search/variables_e.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['udcommands',['UDCommands',['../d0/d3b/classcommands_1_1_user_made_utilities.html#a69a032f45b3ba831667efe68bf967d80',1,'commands::UserMadeUtilities']]],
-  ['userdefined',['userdefined',['../d0/da7/classconfiguration_1_1_basic_parser.html#aa12d7ff59e61540988038a144c8b2b51',1,'configuration::BasicParser']]]
-];
