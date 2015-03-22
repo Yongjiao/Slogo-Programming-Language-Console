a205d4f3b935bc997959627ba59252ab98552485
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-22 03:33:20 +0000

    Added Readme

--- Doxygen/html/search/classes_13.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['webpopup',['WebPopUp',['../dd/d85/classgui_1_1_web_pop_up.html',1,'gui']]]
-];
--- Doxygen/html/search/files_12.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['view_2ejava',['View.java',['../d0/d27/_view_8java.html',1,'']]],
-  ['viewcommands_2ejava',['ViewCommands.java',['../dc/d8a/_view_commands_8java.html',1,'']]],
-  ['viewhandler_2ejava',['ViewHandler.java',['../d2/d03/_view_handler_8java.html',1,'']]],
-  ['viewhandler2_2ejava',['ViewHandler2.java',['../d8/d9c/_view_handler2_8java.html',1,'']]]
-];
