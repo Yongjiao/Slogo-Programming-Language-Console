c9159dfc08c8722f59da295ebe3ca970b11d7977
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-22 03:15:50 +0000

    Added Readme

--- Doxygen/html/search/variables_2.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['dashed',['DASHED',['../d0/d9b/enumapplication_1_1_pen_1_1_p_e_n_s_t_y_l_e.html#a24352b60f999db5141126e47da2bf241',1,'application::Pen::PENSTYLE']]],
-  ['default_5fbackground_5fcolor',['DEFAULT_BACKGROUND_COLOR',['../d6/d2f/classgui_1_1_g_u_i.html#aaa277f84ef8220e7b6278770092897b1',1,'gui::GUI']]],
-  ['default_5fcolor',['DEFAULT_COLOR',['../d7/d9b/classgui_1_1_background_view.html#ac83d4bfa37201f65821a26797a650388',1,'gui::BackgroundView']]],
-  ['default_5fpen_5fcolor',['DEFAULT_PEN_COLOR',['../d6/d2f/classgui_1_1_g_u_i.html#a3edd80156d0171be843ea4702921e4b5',1,'gui.GUI.DEFAULT_PEN_COLOR()'],['../d8/ddb/classgui_1_1_line_view.html#a9936431f32c31688b6b88b15d5659e69',1,'gui.LineView.DEFAULT_PEN_COLOR()']]],
-  ['default_5fpoint',['DEFAULT_POINT',['../d8/d8b/classgui_1_1_view.html#a7ab62d621e120977c3b713509fe1a44e',1,'gui::View']]],
-  ['default_5fturltle_5fimage',['DEFAULT_TURLTLE_IMAGE',['../d3/de0/classapplication_1_1_view_handler2.html#aca045a32b9217c714e49455edc952a48',1,'application::ViewHandler2']]]
-];
